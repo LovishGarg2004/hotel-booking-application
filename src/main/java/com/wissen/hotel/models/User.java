c8a78167519eb2aefe58104b42b6ab9118aa5d80
Jashit Goyal <jashitgoyal@Jashits-Laptop.local>
2025-05-11 15:08:16 +0530

    Setting up hibernate

--- src/main/java/com/wissen/hotel/models/User.java
+++ src/main/java/com/wissen/hotel/models/User.java
@@ -5,17 +5,21 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.annotations.UuidGenerator;
-
+import com.wissen.hotel.enums.UserRole;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
